673258fd3b3a92a34bb919b15918eb2b2a8f98c2
cafferti <saulkumven@gmail.com>
2023-09-02 00:45:16 +0100

    created react app

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -2,35 +2,22 @@
 
 
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const title = `welcome to new blog`
+  const likes = 50
+  const link = `http://paletton.com/#uid=13w0u0kllllaFw0g0qFqFg0w0aF`
   return (
-    <div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='App' >
+     <div className="content">
+      <h1>{title}</h1>
+      <h2>{likes} times</h2>
+      <p>{ 70 }</p>
+      <p>{ Math.random() * 5}</p>
+      <p>{[1,2,3,4,5,6]}</p>
+      <a href={link}>palleton site</a>
+     </div>
     </div>
   )
 }
