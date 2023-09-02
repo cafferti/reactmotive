6e288f897cfd1852727696e89cbc12e720ab4a31
cafferti <saulkumven@gmail.com>
2023-09-02 23:00:56 +0100

    lesson8 usestates and devtools

--- reactapp/src/home.jsx
+++ reactapp/src/home.jsx
@@ -1,20 +1,31 @@
-const Home  = () => {
-    const handleclick = ()=>{
-        console.log(`hello ninjas`)
-    }    
+import { useState } from "react";
 
-    const handleclickAgain = name =>{
-        console.log(`hello ${name}`)
+const Home = () => {
+  let [count,setCount] = useState(0);
+  // let username = `austin`
+  const [name, setName] = useState("mario");
+  const [age, setAge] = useState(25);
+  const handleclick = () => { 
+    if(count === 0){
+        setName("luigi");
+        setAge(30)
+        setCount(1)
+    }else{
+        setName('mario')
+        setAge(25)
+        setCount(0)
     }
-    const username = `austin`
-    return ( 
-        <div className="home">
-        <h2>Home Page</h2>
-        <button onClick ={handleclick} >click me</button>
-        <button onClick={() =>{handleclickAgain(username)}} >click me Again</button>
-        </div>
-        
-     );
-}
- 
-export default Home ;
+  
+   
+  };
+
+  return (
+    <div className="home">
+      <h2>Home Page</h2>
+      <p>{name} is {age} years old</p>
+      <button onClick={handleclick}>click me</button>
+    </div>
+  );
+};
+
+export default Home;
