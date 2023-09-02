fbd6ce7eebdd1059bbfd5dc07250e21704c3176e
cafferti <saulkumven@gmail.com>
2023-09-02 02:06:11 +0100

    created react app and created component

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,7 +1,6 @@
 
 
 
-import './App.css'
 import Navbar from './navbar';
 import Home from './home'
 
--- reactapp/src/navbar.jsx
+++ reactapp/src/navbar.jsx
@@ -6,7 +6,11 @@ const Navbar = () => {
       <h1>The Dojo act</h1>
       <div className="links">
         <a href="/create">Home</a>
-        <a href="/create">New Blog</a>
+        <a href="/create" style={{
+          color:"white",
+          backgroundColor:'#f1356d',
+          borderRadius:'8px',
+        }}>New Blog</a>
       </div>
     </nav>
   );
