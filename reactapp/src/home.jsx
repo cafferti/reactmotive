50a0755fbbb47552842d8e61ef07b646b8366b60
cafferti <saulkumven@gmail.com>
2023-09-04 13:49:23 +0100

    lesson 14 useeffects and dependacies

--- reactapp/src/home.jsx
+++ reactapp/src/home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
      {title: 'Welcome home', body:`lorem....`,author:'Yoshi',id: 2 },
      {title: 'web dev tips', body:`lorem....`,author:'mario',id: 3 }   
     ])
+    const [name,setName] = useState(`mario`)
 
     const handleDelete = (id) => {
         const newblog = blogs.filter( (blogs) => blogs.id !== id)
@@ -16,11 +17,13 @@ const Home = () => {
      
 useEffect(() =>{
     console.log('use effect running')
-    console.log(blogs)
-}) 
+    console.log(name)
+}, [name]) 
     return (
     <div className="home">
         <Bloglist blogs={blogs} title = 'All Blogs!' handleDelete ={handleDelete}/>
+        <button onClick={() => setName('luigi')} >change name</button>
+        <p>{name}</p>
     </div>
   );
 };
