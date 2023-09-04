0ec87844cfc63c45c3970c021da4411a20caa2d6
cafferti <saulkumven@gmail.com>
2023-09-04 14:28:17 +0100

    lesson 16 created dummy server

--- reactapp/src/home.jsx
+++ reactapp/src/home.jsx
@@ -2,28 +2,27 @@ import { useState, useEffect } from "react";
 import Bloglist from "./bloglist";
 
 const Home = () => {
- 
-    const [blogs,setBlogs] = useState([
-     {title: 'my new website', body:`lorem....`,author:'mario',id: 1  },
-     {title: 'Welcome home', body:`lorem....`,author:'Yoshi',id: 2 },
-     {title: 'web dev tips', body:`lorem....`,author:'mario',id: 3 }   
-    ])
-    const [name,setName] = useState(`mario`)
+  const [blogs, setBlogs] = useState([
+    { title: "my new website", body: `lorem....`, author: "mario", id: 1 },
+    { title: "Welcome home", body: `lorem....`, author: "Yoshi", id: 2 },
+    { title: "web dev tips", body: `lorem....`, author: "mario", id: 3 },
+  ]);
+  const [name, setName] = useState(`mario`);
 
-    const handleDelete = (id) => {
-        const newblog = blogs.filter( (blogs) => blogs.id !== id)
-        setBlogs(newblog)
-    }
-     
-useEffect(() =>{
-    console.log('use effect running')
-    console.log(name)
-}, [name]) 
-    return (
+  const handleDelete = (id) => {
+    const newblog = blogs.filter((blogs) => blogs.id !== id);
+    setBlogs(newblog);
+  };
+
+  useEffect(() => {
+    console.log("use effect running");
+    console.log(name);
+  }, []);
+  return (
     <div className="home">
-        <Bloglist blogs={blogs} title = 'All Blogs!' handleDelete ={handleDelete}/>
-        <button onClick={() => setName('luigi')} >change name</button>
-        <p>{name}</p>
+      <Bloglist blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName("luigi")}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
