2c7355795cbeb95ea6475df028c11add00a6e78a
cafferti <saulkumven@gmail.com>
2023-09-04 23:42:23 +0100

    lesson 19 error handling  with Api in react

--- reactapp/src/home.jsx
+++ reactapp/src/home.jsx
@@ -2,23 +2,42 @@ import { useState, useEffect } from "react";
 import Bloglist from "./bloglist";
 
 const Home = () => {
-  const [isPending, setPending] = useState(true)
+  const [isPending, setPending] = useState(true);
   const [blogs, setBlogs] = useState(null);
   const [name, setName] = useState(`mario`);
+  const [error, setError] = useState("null");
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-    .then(response =>{
+    fetch("http://localhost:8000/blogs")
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(`failed to fetch data from that resourse `);
+        }
         return response.json();
-    })
-    .then(  (data) =>{
-       console.log(data) 
-       setBlogs(data)
-       setPending(false)
-    })
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+        setPending(false);
+        setError(null)
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+    setPending(false);
   }, []);
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>loading....</div>}
+      {blogs && <Bloglist blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
 
-  //using async function/
+export default Home;
+
+//using async function/
 //   useEffect(() => {
 //     (async () => {
 //       const response = await fetch('http://localhost:8000/blogs');
@@ -26,13 +45,3 @@ const Home = () => {
 //       setBlogs(data);
 //     })();
 //   }, []);
-
-  return (
-    <div className="home">
-        {isPending && <div>loading....</div>}
-    { blogs && <Bloglist blogs={blogs} title="All Blogs!"  />}  
-    </div>
-  );
-};
-
-export default Home;
