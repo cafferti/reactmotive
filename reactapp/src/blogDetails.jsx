bd838bac18064d4a0b6be264e68c39044d7cc2a7
cafferti <saulkumven@gmail.com>
2023-09-05 11:19:09 +0100

    lesson 26 reusing custom hookss (usefetch)

--- reactapp/src/blogDetails.jsx
+++ reactapp/src/blogDetails.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import usefetch  from "./usefetch";
 
 
 const blogDetails = () => { useParams
     const { id } = useParams()
+    const {fresult:blog , error, isPending} = usefetch("http://localhost:8000/blogs" + id)
     return ( 
         <div className="blogDetails">
-            <h2>Blog details - { id }</h2>
+            {isPending && <div>loading...</div>}
+            {error && <div>{error}</div>}
+            { blog && (
+               <article>
+                <h2>{blog.title}</h2>
+                <p>written by {blog.author}</p>
+                <div>{blog.body}</div>
+               </article> 
+            ) }
         </div>
      );
 }
