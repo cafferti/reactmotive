84fa74f7460bc39a2511532991beb7f2e7b1cffc
cafferti <saulkumven@gmail.com>
2023-09-05 12:12:07 +0100

    lesson 27 use of forms

--- reactapp/src/createblog.jsx
+++ reactapp/src/createblog.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
-        <div className="create">
-            <h2>Add new Blog</h2>
-        </div>
-     );
-}
- 
-export default Create ;
+const [title, setTitle] = useState('')
+const [body, setBody] = useState('')
+const [author, setAuthor] = useState('mario')
+
+  return (
+    <div className="create">
+      <h2>Add new Blog</h2>
+      <form>
+        <label> Blog Details</label>
+        <input type="text"
+         required 
+         value={title}
+         onChange={(e) => setTitle(e.target.value)}/>
+
+        <label> Blog Body</label>
+        <textarea
+         required 
+         value={body}
+         onChange={(e) => setBody(e.target.value)}/>
+
+        <label>Blog Author</label>
+        <select
+        value={{author}}
+        onChange={(e) => setAuthor(e.target.value) }>
+            <option value= "mario" >mario</option>
+            <option  value= "yoshi" >Yoshi</option>
+        </select>
+        <button>Add button</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
+    </div>
+  );
+};
+
+export default Create;
