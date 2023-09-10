37c8fb805245fabfc55947a3c0e2799fe34a4562
cafferti <saulkumven@gmail.com>
2023-09-10 23:02:23 +0100

    completed layout

--- reactapp2/src/Marsintro.jsx
+++ reactapp2/src/Marsintro.jsx
@@ -4,7 +4,7 @@ const marsintro = () => {
   return (
     <div>
       <h1 className="marslogo"> 01 Pick your destination</h1>
-      <img src={marslogo} alt="" width={"50%"} />
+      <img src={marslogo} alt="" width={"60%"} />
     </div>
   );
 };
--- reactapp2/src/nestednav.jsx
+++ reactapp2/src/nestednav.jsx
@@ -12,15 +12,16 @@ const items = ['Moon', 'Mars', 'Europa', 'Titans'];
 
   return (
     <div className="nav">
-      <ul>
-        {items.map((item) => ( <li key={item} onClick={() => handleItemClick(item)} className={activeItem === item ? 'active' : ''}> {item}
-          </li>))}
-     </ul>
          <div className="marsabout">
             <div className="part1">
             <Marsintro/>
          </div>
             <div className="part2">
+            <ul>
+        {items.map((item) => ( <li key={item} onClick={() => handleItemClick(item)} className={activeItem === item ? 'active' : ''}> {item}
+          </li>))}
+     </ul>
+
             <Marsstats/>
             </div>
        
