67cd222d28b04fdecd73d78829aa969b248de556
cafferti <saulkumven@gmail.com>
2023-09-10 22:57:11 +0100

    debugging nestednavbar and layout

--- reactapp2/src/Marsintro.jsx
+++ reactapp2/src/Marsintro.jsx
@@ -4,7 +4,7 @@ const marsintro = () => {
   return (
     <div>
       <h1 className="marslogo"> 01 Pick your destination</h1>
-      <img src={marslogo} alt="" width={"70%"} />
+      <img src={marslogo} alt="" width={"50%"} />
     </div>
   );
 };
