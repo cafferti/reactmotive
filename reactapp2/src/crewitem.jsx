7e7bc012a82c1ce12644afffff83e2eda0927114
cafferti <saulkumven@gmail.com>
2023-09-18 12:26:05 +0100

    carousel app set up and added swipe class using setinterval

--- reactapp2/src/crewitem.jsx
+++ reactapp2/src/crewitem.jsx
@@ -3,14 +3,29 @@ import { useEffect, useState } from "react";
 const Carouselitem = ({ crewmember }) => {
 
   const [activecrew, Setactivecrew] = useState(0)
+  const [activeclass, Setactiveclass] = useState('')
+
+
+const swipeclass = () =>{
+ const ourin = setInterval(() =>{
+    Setactiveclass('swipe')
+
+  }, 3000)
+}
+
 useEffect(() =>{
  const intervalid = setInterval(() =>{
+    swipeclass()
     if( activecrew < crewmember.length -1){
+      Setactiveclass('')
+
       Setactivecrew(activecrew + 1)
     }else if(activecrew === crewmember.length -1){
+      Setactiveclass('')
       Setactivecrew(0)
     }
   }, 3000)
+
 
   return () =>(
     clearInterval(intervalid)
@@ -21,7 +36,7 @@ useEffect(() =>{
 
 
   return (
-    <div>
+    <div className={activeclass}>
       <h2>{crewmember[activecrew].Role}</h2>
       <h1>{crewmember[activecrew].Name}</h1>
       <p>{crewmember[activecrew].About}</p>
